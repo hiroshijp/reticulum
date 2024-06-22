d47411171cdc8d71da4c03b705641b42388e2086
hiroshijp <hiroshijp0616@gmail.com>
2024-06-22 18:32:58 +0900

    [update] WIP

--- lib/ret/hub_logger.ex
+++ lib/ret/hub_logger.ex
@@ -1,13 +1,14 @@
 defmodule Ret.HubLogger do
-  @idle_timeout 5000
   use GenServer, restart: :temporary
   alias PhoenixClient.{Socket, Message}
 
   require Logger
 
+  @timeout_interval 10000
+
   def start_link({hub_sid}) do
     name = {:via, Registry, {Ret.Registry, "#{hub_sid}"}}
-    GenServer.start_link(__MODULE__, {hub_sid}, name: name, [timeout: @idle_timeout])
+    GenServer.start_link(__MODULE__, {hub_sid}, [name: name, timeout: 30000])
   end
 
   def init({hub_sid}) do
@@ -17,23 +18,24 @@ defmodule Ret.HubLogger do
       "hub:#{hub_sid}",
       %{"profile" => "hub_logger"}
     )
-    {:ok, hub_sid, file}
+    {:ok, {hub_sid, file}}
   end
 
   def handle_info(:timeout, {hub_sid, file}) do
-    Logger.info("miyoshi: self:#{inspect(self())} file:#{inspect(file)}")
-    IO.write(file, "Timeout\n")
-    File.close(file)
-    Registry.unregister(Ret.Registry, hub_sid)
-    {:stop, :normal, :ok}
+    {:stop, :timeout, {hub_sid, file}}
   end
 
-  def handle_info(
-    %PhoenixClient.Message{
-      topic: hub_sid,
-      event: "nafr",
-    } = msg, {hub_sid, file}) do
-    IO.write(file, "#{inspect(msg)}\n")
+  def handle_info(msg, {hub_sid, file}) do
+    IO.write(file, "#{inspect(hub_sid)}: #{inspect(msg)}\n")
     {:noreply, {hub_sid, file}}
+  end
+
+  def terminate(reason, {hub_sid, file}) do
+    Logger.info("miyoshi")
+
+    IO.write(file, "reason: #{reason} Terminated\n")
+    File.close(file)
+    Registry.unregister(Ret.Registry, hub_sid)
+    {:shutdown, reason}
   end
 end
