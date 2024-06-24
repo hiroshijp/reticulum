3f536b7525a77989126a386df1ff28469923eb2f
hiroshijp <hiroshijp0616@gmail.com>
2024-06-24 10:44:04 +0900

    [update]  add timeout for logger process

--- lib/ret/hub_logger.ex
+++ lib/ret/hub_logger.ex
@@ -4,15 +4,17 @@ defmodule Ret.HubLogger do
 
   require Logger
 
-  @timeout_interval 10000
+  # TODO: 本当に60秒か要検証
+  @timeout_interval 60_000
 
   def start_link({hub_sid}) do
     name = {:via, Registry, {Ret.Registry, "#{hub_sid}"}}
-    GenServer.start_link(__MODULE__, {hub_sid}, [name: name, timeout: 30000])
+    GenServer.start_link(__MODULE__, {hub_sid}, [name: name, timeout: @timeout_interval])
   end
 
   def init({hub_sid}) do
-    {:ok, file} = File.open("./logs/#{hub_sid}.log", [:write, :delayed_write])
+    {:ok, file} = File.open("./logs/#{hub_sid}.log", [:write, :delayed_write, :append])
+    IO.write(file, "Start logging for hub: #{hub_sid}\n")
 
     # make hub logger join to hub channel
     {:ok, _res, _channel} = PhoenixClient.Channel.join(
@@ -21,24 +23,26 @@ defmodule Ret.HubLogger do
       %{"profile" => "hub_logger"}
     )
 
-    mew_timer = set_timer(@timeout_interval)
+    new_timer = set_timer(@timeout_interval)
 
-    {:ok, [hub_sid, file, new_imer]}
+    {:ok, [hub_sid, file, new_timer]}
   end
 
   # handle timeout
-  def handle_info(:timeout, [hub_sid, file, new_imer]) do
-    {:stop, :timeout, [hub_sid, file, new_imer]}
+  def handle_info(:timeout, [hub_sid, file, new_timer]) do
+    Logger.info("miyoshi throygh handleinfo :timeout")
+    {:stop, :timeout, [hub_sid, file, new_timer]}
   end
 
   # hundale all messages
-  def handle_info(msg, [hub_sid, file, new_imer]) do
+  def handle_info(msg, [hub_sid, file, new_timer]) do
     IO.write(file, "#{inspect(msg)}\n")
     new_imer = set_timer(@timeout_interval)
-    {:noreply, [hub_sid, file, new_imer]}
+    {:noreply, [hub_sid, file, new_timer]}
   end
 
-  def terminate(reason, [hub_sid, file, new_imer]) do
+  def terminate(reason, [hub_sid, file, new_timer]) do
+    Logger.info("miyoshi throygh terminated :timeout")
     IO.write(file, "reason: #{reason} Terminated\n")
     File.close(file)
     Registry.unregister(Ret.Registry, hub_sid)
@@ -46,6 +50,6 @@ defmodule Ret.HubLogger do
   end
 
   defp set_timer(timeout_interval) do
-    new_imer = Process.send_after(self(), :timeout, timeout_interval)
+    new_timer = Process.send_after(self(), :timeout, timeout_interval)
   end
 end
